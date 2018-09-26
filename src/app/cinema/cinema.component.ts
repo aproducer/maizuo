7cbf70e721ba658145b7d3f4013921dda6caa73c
aproducer <906294418@qq.com>
2018-09-26 21:15:18 +0800

    影院界面完成 cinemaPage complete

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-cinema',
-  templateUrl: './cinema.component.html',
-  styleUrls: ['./cinema.component.css']
+	selector: 'app-cinema',
+	templateUrl: './cinema.component.html',
+	styleUrls: ['./cinema.component.css']
 })
 export class CinemaComponent implements OnInit {
+	cinemas = {}
+	districts=[];
+	constructor(private http: Http) {}
 
-  constructor() { }
+	ngOnInit() {
+		this.http.get("/v4/api/cinema?__t=1537962319155").subscribe(reg => {
+			console.log(reg.json().data.cinemas);
+			reg.json().data.cinemas.forEach((cinema) => {
+				if(!this.cinemas[cinema.district.name]) {
+					this.cinemas[cinema.district.name] = [];
+					this.cinemas[cinema.district.name].show=false;
+				}
+				this.cinemas[cinema.district.name].push(cinema);
+			});
+			this.districts=Object.keys(this.cinemas);
+			console.log(this.cinemas,this.districts);
+		});
 
-  ngOnInit() {
-  }
-
+	}
+	show(str){
+		console.log(str,this.cinemas[str].show);
+		this.cinemas[str].show=!this.cinemas[str].show;
+	}
 }
