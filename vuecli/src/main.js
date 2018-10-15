be0294e0d9fcc6e946b005f641cda416dfd8a1cf
aproducer <906294418@qq.com>
2018-10-15 09:47:40 +0800

    引入ElementUI + MintUI

--- vuecli/src/main.js
+++ vuecli/src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(MintUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
+  render: h => h(App),
   template: '<App/>'
 })
